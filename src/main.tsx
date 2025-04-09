d08b6ce9e4fdbeeff84f1ea6034318c7433e7bc5
Cameron <cameronhunt88@hotmail.com>
2025-04-09 12:02:59 +0100

    Add global font

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
-)
+// Styles
+import '../src/styles/main.scss'
+
+createRoot(document.getElementById('root')!).render(<StrictMode></StrictMode>)
