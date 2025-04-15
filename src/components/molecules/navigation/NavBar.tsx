d23babbffee8d3ec8b6fb09bc32ddc824f1fa390
CamyH <cameronhunt88@hotmail.com>
2025-04-15 15:31:08 +0100

    Update Header/Navbar to work with hamburger

--- src/components/molecules/navigation/NavBar.tsx
+++ src/components/molecules/navigation/NavBar.tsx
@@ -6,10 +6,21 @@ import styles from '../../../styles/molecules/_navBar.module.scss'
 
 type NavBarPropType = {
     children: ReactNode
+    isMobile: boolean
+    isOpen: boolean
 }
 
-export const NavBar = ({ children }: NavBarPropType) => {
-    return <nav className={styles.navbar}>{children}</nav>
+export const NavBar = ({ children, isMobile, isOpen }: NavBarPropType) => {
+    const classNames = [
+        styles.navbar,
+        isMobile && styles.navbarMobile,
+        isMobile && !isOpen && styles.hideNavBar,
+        isMobile && isOpen && styles.showNavBar,
+    ]
+        .filter(Boolean)
+        .join(' ')
+
+    return <nav className={classNames}>{children}</nav>
 }
 
 export default NavBar
--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,21 +1,48 @@
+// hooks
+import { useIsMobile } from '../../../@hooks'
+import { useState } from 'react'
+
 // Components
-import { Heading, Headshot, NavBarLink } from '../../atoms'
+import { HamburgerButton, Heading, Headshot, NavBarLink } from '../../atoms'
 import { NavBar } from '../../molecules'
 
 // Styles
 import styles from '../../../styles/organisms/_header.module.scss'
 
 export const Header = () => {
+    const isMobile = useIsMobile()
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOpenMobileNavigation = () =>
+        setIsOpen((prevValue) => !prevValue)
+
     return (
-        <header className={styles.header}>
-            <Headshot />
-            <Heading as='h1' children={'Cameron Hunt'} />
-            <NavBar>
-                <NavBarLink linkText={'about'} path={''} />
-                <NavBarLink linkText={'portfolio'} path={''} />
-                <NavBarLink linkText={'resume'} path={''} />
-            </NavBar>
-        </header>
+        <>
+            <header className={styles.header}>
+                <Headshot />
+                <Heading
+                    as={isMobile ? 'h2' : 'h1'}
+                    children={'Cameron Hunt'}
+                />
+                {isMobile ? (
+                    <HamburgerButton onClick={handleOpenMobileNavigation} />
+                ) : (
+                    <NavBar isMobile={isMobile} isOpen={isOpen}>
+                        <NavBarLink linkText={'about'} path={''} />
+                        <NavBarLink linkText={'portfolio'} path={''} />
+                        <NavBarLink linkText={'resume'} path={''} />
+                    </NavBar>
+                )}
+            </header>
+            {(isMobile || isOpen) && (
+                <NavBar isMobile={isMobile} isOpen={isOpen}>
+                    <NavBarLink linkText={'about'} path={''} />
+                    <NavBarLink linkText={'portfolio'} path={''} />
+                    <NavBarLink linkText={'resume'} path={''} />
+                </NavBar>
+            )}
+        </>
     )
 }
 
